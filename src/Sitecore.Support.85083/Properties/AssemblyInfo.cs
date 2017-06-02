866ab1048ae7929f985d12a20c316a8438ecc335
Dmitry Baranov <dbr@sitecore.net>
2017-06-02 18:57:09 +0300

    no message

---
+++ src/Sitecore.Support.85083/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.85083")]
+[assembly: AssemblyProduct("Sitecore.Support.85083")]
+[assembly: ComVisible(false)]
